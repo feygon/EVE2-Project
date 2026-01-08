5f51c14711097b041549d2ddb0baa4249941566f
Feygon Nickerson <feygon@gmail.com>
2026-01-08 14:04:51 -0800

    fix: use env var for DB_USER now that DirectAdmin settings are saved

--- dbcon.js
+++ dbcon.js
@@ -6,7 +6,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : process.env.DB_HOST || 'localhost',
   port            : process.env.DB_PORT || 3306,
-  user            : 'realfey_realfey_realfeyuser',  // Hardcoded temporarily to bypass env var issues
+  user            : process.env.DB_USER || 'realfey_realfey_realfeyuser',
   password        : process.env.DB_PASSWORD,
   database        : process.env.DB_NAME || 'realfey_realfey_eve2_project',
   multipleStatements: true
